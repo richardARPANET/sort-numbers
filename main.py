4e036ec6a1855f0e6e15289ea1129e76098d426b
richard <richard@richard.do>
2013-07-04 20:06:29 +0100

    Initial commmit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-numbers = [2, 1, 3, 3, 8, 9, 4, 5, 6, 7]
+nums = [2, 1, 3, 3, 8, 9, 4, 5, 6, 7]
 sorted = []
 
 
 def sort():
     #sort numbers ascending order
-    temp = numbers
+    temp = nums
     for i in range(len(temp)):
         for j in range(i+1, len(temp)):
-            if numbers[j] < numbers[i]:
-                numbers[j], numbers[i] = numbers[i], numbers[j]
-    return numbers
+            if nums[j] < nums[i]:
+                nums[j], nums[i] = nums[i], nums[j]
+    return nums
 
 if __name__ == '__main__':
     print sort()
